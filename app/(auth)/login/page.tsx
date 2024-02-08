a2e5ea0ef74c3268787f32cd6fe5cba9b650d143
Zeyad Lotfy <zezoramy380@gmail.com>
2024-02-08 05:47:47 +0200

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@components/AuthForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <AuthForm type="login" />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@components/AuthForm";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <AuthForm type="register" />
+    </div>
+  );
+};
+
+export default RegisterPage;
